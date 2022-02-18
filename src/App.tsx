1cdf560e727f03b0f3f5bff087d7e3ac43bbff16
marzeq <58303665+marzeq@users.noreply.github.com>
2022-02-18 10:15:48 +0100

    Use type generics for React#useState instead of type assertions

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App(path: Games) {
 		[revealed, setRevealed] = React.useState(false),
 		[showTip, setTip] = React.useState(false),
 		[confirmTip, setConfirmTip] = React.useState(false),
-		[letters, setLetters] = React.useState(1) as [LetterCount, Dispatch<SetStateAction<number>>],
+		[letters, setLetters] = React.useState<LetterCount>(1),
 		dayWord = getWordForDate(date, path),
 		tip = generateTip(dayWord, letters, path),
 		{ epoch } = require(`./util/${path}/util`) as GameData,
